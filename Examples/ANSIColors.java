1f9ba756b566ee7ce29ca15aabd64be8bfd07fb6
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-24 18:51:21 -0300

    ANSI Colors

---
+++ Examples/ANSIColors.java
@@ -0,0 +1,16 @@
+/**
+ * ANSI COLORS TEST IN JAVA
+ */
+
+public class ANSIColors{
+    public static void main(String[] args){
+        System.out.println("\033[30m [PRETO]");
+        System.out.println("\033[31m [VERMELHO]");
+        System.out.println("\033[32m [VERDE]");
+        System.out.println("\033[33m [AMARELO]");
+        System.out.println("\033[34m [AZUL]");
+        System.out.println("\033[35m [ROXO]");
+        System.out.println("\033[36m [AZUL CLARO]");
+        System.out.println("\033[37m [BRANCO]");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# TUI-TESTS
-My TUI (Text User Interface) Examples
+# TEXT USER INTERFACE EXAMPLES
+
+### Colors in Java
+
+ANSI CODE|COLOR|CODE EXAMPLE
+|---|---|---|
+`\033[30m`|Black|
+```java 
+System.out.println("\033[30m [BLACK]");
+```
+`\033[31m`|Red|
+```java
+System.out.println("\033[31m [RED]");
+```
