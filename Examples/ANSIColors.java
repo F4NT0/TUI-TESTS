eb59c5c7d24208baa49f683c2ec010192dae7147
F4NT0 <gabriel.stundner@acad.pucrs.br>
2020-10-24 18:58:24 -0300

    ANSI Colors

--- Examples/ANSIColors.java
+++ Examples/ANSIColors.java
@@ -4,13 +4,13 @@
 
 public class ANSIColors{
     public static void main(String[] args){
-        System.out.println("\033[30m [PRETO]");
-        System.out.println("\033[31m [VERMELHO]");
-        System.out.println("\033[32m [VERDE]");
-        System.out.println("\033[33m [AMARELO]");
-        System.out.println("\033[34m [AZUL]");
-        System.out.println("\033[35m [ROXO]");
-        System.out.println("\033[36m [AZUL CLARO]");
-        System.out.println("\033[37m [BRANCO]");
+        System.out.println("\033[30m [BLACK]");
+        System.out.println("\033[31m [RED]");
+        System.out.println("\033[32m [GREEN]");
+        System.out.println("\033[33m [YELLOW]");
+        System.out.println("\033[34m [BLUE]");
+        System.out.println("\033[35m [PURPLE]");
+        System.out.println("\033[36m [LIGHT BLUE]");
+        System.out.println("\033[37m [WHITE]");
     }
 }
--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 
 ANSI CODE|COLOR|CODE EXAMPLE
 |---|---|---|
-`\033[30m`|Black|
-```java 
-System.out.println("\033[30m [BLACK]");
-```
-`\033[31m`|Red|
-```java
-System.out.println("\033[31m [RED]");
-```
+`\033[30m`|Black|**System.out.println("\033[30m [BLACK]");**
+`\033[31m`|Red|**System.out.println("\033[31m [RED]");**
+`\033[32m`|Green|**System.out.println("\033[32m [GREEN]");**
+`\033[33m`|Yellow|**System.out.println("\033[33m [YELLOW]");**
+`\033[34m`|Blue|**System.out.println("\033[34m [BLUE]");**
+`\033[35m`|Purple|**System.out.println("\033[35m [PURPLE]");**
+`\033[36m`|Light Blue|**System.out.println("\033[36m [LIGHT BLUE]");**
+`\033[37m`|White|**System.out.println("\033[37m [WHITE]");**
